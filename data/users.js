a9c9758706eec4319dbf05c4a52c8b487e33f1bb
Anderson Saraiva <a.saraiva@biobe.com.br>
2022-08-09 10:23:04 -0300

    first commit

---
+++ data/users.js
@@ -0,0 +1,55 @@
+const RandomId = require("../helpers/utils");
+
+const users = [
+  {
+    id: RandomId,
+    name: "Tom Holland",
+    email: "t.holland@email.com",
+    username: "holland",
+    age: 26,
+  },
+  {
+    id: RandomId,
+    name: "Chris Evans",
+    email: "c.evans@email.com",
+    username: "evans",
+    age: 41,
+  },
+  {
+    id: RandomId,
+    name: "Chris Hemsworth",
+    email: "c.hemsworth@email.com",
+    username: "andre",
+    age: 38,
+  },
+  {
+    id: RandomId,
+    name: "Scarlett Johansson",
+    email: "s.johansson@email.com",
+    username: "johansson",
+    age: 37,
+  },
+  {
+    id: RandomId,
+    name: "Mark Ruffalo",
+    email: "m.ruffalo@email.com",
+    username: "ruffalo",
+    age: 54,
+  },
+  {
+    id: RandomId,
+    name: "Jeremy Renner",
+    email: "j.renner@email.com",
+    username: "renner",
+    age: 51,
+  },
+  {
+    id: RandomId,
+    name: "Robert Downey Jr",
+    email: "r.downey@email.com",
+    username: "downey",
+    age: 57,
+  },
+];
+
+module.exports = users;
